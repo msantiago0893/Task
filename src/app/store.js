5e639e6318988ad2b29e4ec113b6f1dd4f0d3119
Marco Antonio <msantiago@linko.mx>
2022-11-24 15:59:32 -0600

    feat(app): add store

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
+import taskReducer from '../features/task/taskSlice'
 
+// * Lo que espera el store son los reducers
 export const store = configureStore({
-
+  reducer: {
+    task: taskReducer
+  }
 })
