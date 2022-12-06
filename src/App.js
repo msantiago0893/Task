21d2d37611c150c7622d6fa07281d046e9697979
Marco Antonio <msantiago@linko.mx>
2022-12-06 14:20:46 -0600

    refactor(app): clean code

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import './App.css';
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 import TaskForm from './Components/TaskForm';
 import TaskList from './Components/TaskList';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <TaskList />
-        <TaskForm />
-      </header>
+    <div className="bg-zinc-900 h-screen text-white">
+      <div className='flex items-center justify-center h-full'>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<TaskList />} />
+            <Route path='/create-task' element={<TaskForm />} />
+            <Route path='/edit-task/:id' element={<TaskForm />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
     </div>
   );
 }
--- src/app/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import taskReducer from '../features/task/taskSlice'
-
-// * Lo que espera el store son los reducers
-export const store = configureStore({
-  reducer: {
-    task: taskReducer
-  }
-})
