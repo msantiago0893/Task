7cfe86c37539fe48473cbe6ade8e940ef1b5207f
Marco A. Santiago Feria <94033822+msantiago0893@users.noreply.github.com>
2023-10-07 15:17:31 -0600

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import taskReducer from '../slices/task/taskSlice'
 
-// * Lo que espera el store son los reducers
 export const store = configureStore({
   reducer: {
     task: taskReducer
